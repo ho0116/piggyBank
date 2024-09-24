d99898828ea24b6a2f886636e26aebac32b39d5c
name1116 <milk100409@gmail.com>
2024-09-24 17:01:11 +0900

    SC 중간 전송

---
+++ frontend/src/app/challenge/detail/[id]/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function detailPage () {
+    return (<>
+        <p>상세페이지</p>
+    </>)
+}
--- frontend/src/app/myPage/challengeList/page.tsx
+++ frontend/src/app/myPage/challengeList/page.tsx
@@ -1,45 +1,73 @@
-"use client"
+"use client";
 
 import { useQuery } from "@tanstack/react-query";
 import { getAllChallenges } from "@/app/api/challengeApi";
 import challenge from "@/app/types/challengeType";
 import useAuth from "@/app/hooks/useAuth";
+import Link from "next/link";
 
-export default function ChallengeList(){
+export default function ChallengeList() {
+  // const [list, setList] = useState([]);
+  const { user } = useAuth();
 
-    // const [list, setList] = useState([]);
-    const {user} = useAuth();
-    
-    const {data:challengeList, isLoading, isError, error} = useQuery({
-        queryKey:["challengeList"],
-        queryFn: ()=> getAllChallenges(user?.id as number),
-        enabled: !!user?.id,
-    })
+  const {
+    data: challengeList,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["challengeList"],
+    queryFn: () => getAllChallenges(user?.id as number),
+    enabled: !!user?.id,
+  });
 
-    console.log(user?.id, challengeList)
+  console.log(user?.id, challengeList);
 
-    return (
-        <div>
-            <p className="font-bold text-2xl text-center pt-5 text-cyan-500">챌린지 리스트</p>
-            {isLoading && <div>Loading</div>}
-            {isError && <div>{error.message}</div>}
-            {!isError && challengeList && 
-        
-            <ul className="max-h-[80%] overflow-y-scroll list-none mt-4 flex flex-col items-center">
-            {challengeList.map((c: challenge) => (
-              <li key={c.id} className="py-4 px-2 bg-white rounded-2xl w-10/12 shadow-md shadow-violet-200/20 mb-6">
-                <div>
-                    <p className="font-bold text-lg">{c.challengeName}</p>
-                    <p className="text-sm text-gray-500">{c.startDate} ~ {c.endDate}</p>
-                    <p className={c.challengeStatus === "In Progress" ? "text-blue-500 w-20 text-sm mb-2" : "bg-red-500  w-20 text-sm mb-2"}>{c.challengeStatus}</p>
-                    {/* <p>현재 저축액 <span className="text-violet-700 font-semibold">{c.savedAmount} 원</span> </p> */}
-                    <p><span>달성률 </span> <span className="text-violet-700 font-semibold">{c.savedAmount/c.targetAmount * 100} %</span> </p>
-                    {/* <p>{c.challengeDescription}</p> */}
-                    <p>{c.challengeDescription}</p>
-                </div>
-              </li>
-            ))}
-          </ul>}
-        </div>
-    )
+  return (
+    <div>
+      <p className="font-bold text-2xl text-center pt-5 text-cyan-500">
+        챌린지 리스트
+      </p>
+      {isLoading && <div>Loading</div>}
+      {isError && <div>{error.message}</div>}
+      {!isError && challengeList && (
+        <ul className="max-h-[80%] overflow-y-scroll list-none mt-4 flex flex-col items-center">
+          {challengeList.map((c: challenge) => (
+            <li
+              key={c.id}
+              className="py-4 px-2 bg-white rounded-2xl w-10/12 shadow-md shadow-violet-200/20 mb-6"
+            >
+              <div>
+                <p className="font-bold text-lg">{c.challengeName}</p>
+                <p className="text-sm text-gray-500">
+                  {c.startDate} ~ {c.endDate}
+                </p>
+                <p
+                  className={
+                    c.challengeStatus === "In Progress"
+                      ? "text-blue-500 w-20 text-sm mb-2"
+                      : "bg-red-500  w-20 text-sm mb-2"
+                  }
+                >
+                  {c.challengeStatus}
+                </p>
+                {/* <p>현재 저축액 <span className="text-violet-700 font-semibold">{c.savedAmount} 원</span> </p> */}
+                <p>
+                  <span>달성률 </span>{" "}
+                  <span className="text-violet-700 font-semibold">
+                    {(c.savedAmount / c.targetAmount) * 100} %
+                  </span>{" "}
+                </p>
+                {/* <p>{c.challengeDescription}</p> */}
+                <p>{c.challengeDescription}</p>
+                <button>
+                  <Link href={`/challenge/detail/${c.id}`}> 상세보기</Link>
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
